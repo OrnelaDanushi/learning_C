2250d5286431e1366935463822a76efff487d8ea
Ornela Danushi <orneladanushi@gmail.com>
2023-04-02 21:36:11 +0200

    Update es9.c

--- Lezione 3/es9.c
+++ Lezione 3/es9.c
@@ -1,4 +1,6 @@
-/*STAMPA RETTANGOLO: Scrivere un programma che stampi un rettangolo di 10 righe e 3 colonne la cui cornice sia costituita da caratteri asterisco e la parte interna da 'X'.
+/*STAMPA RETTANGOLO: 
+Scrivere un programma che stampi un rettangolo di 10 righe e 3 colonne la cui cornice 
+sia costituita da caratteri asterisco e la parte interna da 'X'.
 Output:		***
 		*X*
 		*X*
@@ -9,24 +11,20 @@ Output:		***
 		*X*
 		*X*
 		***		*/
+
 #include<stdio.h>
 int main(){
 	int i=0;
-	if(i==0)
-		printf("***\n");
-	for(i=1; i<9; i++)
-		printf("*X*\n");
-	if(i==9)
-		printf("***\n");
-	return 0;
-}
+	if(i==0)		printf("***\n");
+	for(i=1; i<9; i++)	printf("*X*\n");
+	if(i==9)		printf("***\n");
 
-/*oppure
-int main(){
+	/*oppure
 	printf("***\n");
 	int i=0;
-	for(i=0; i<8; i++)
-		printf("*X*\n");
+	for(i=0; i<8; i++)	printf("*X*\n");
 	printf("***\n");
+	*/
+
 	return 0;
-}	*/
+}
