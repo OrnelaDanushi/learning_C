376c3779e300538c32b0f4f177dadd99faf9826b
Ornela Danushi <orneladanushi@gmail.com>
2023-04-02 21:52:43 +0200

    Update and rename es8.c to es08.c

---
+++ Lezione 4/es08.c
@@ -0,0 +1,13 @@
+/*TABELLINE: 
+Scrivere un programma che, preso in ingresso un numero intero x, 
+stampi (con un numero per riga) la tabellina di x (partendo da x*1 e terminando con x*10)
+Input:	2		Output:	2	4	6	8	10	12	14	16	18	20	*/
+
+#include<stdio.h>
+int main(){
+	int x, i;
+	scanf("%d", &x);
+	for(i=1; i<11; i++)
+		printf("%d\n", x*i);
+	return 0;
+}
