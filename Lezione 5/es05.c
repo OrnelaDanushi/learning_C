f789a18f0f136a2bc2e09081d29aad8c8666256a
Ornela Danushi <orneladanushi@gmail.com>
2023-04-02 22:03:15 +0200

    Update es05.c

--- Lezione 5/es05.c
+++ Lezione 5/es05.c
@@ -1,11 +1,18 @@
-/*METODO BABILONESE: La radice quadrata di un numero z intero non negativo puo' essere calcolata in modo approssimato tramite il metodo detto Babilonese che utilizza la seguente relazione ricorsiva: sqrt(z)=1/2 *(sqrt(z-1) + z/(sqrt(z-1))) se z>0 oppure 1 se z=0. Scrivere un programma che acquisisca da tastiera un intero non negativo e restituisca l'approssimazione della sua radice quadrata calcolata tramite il suddetto metodo implementato tramite una funzione approxsqrt. Il risultato deve essere stampato con esattamente 4 cifre decimali.
+/*METODO BABILONESE: 
+La radice quadrata di un numero z intero non negativo puo' essere calcolata 
+in modo approssimato tramite il metodo detto Babilonese che utilizza la seguente 
+relazione ricorsiva: sqrt(z)=1/2 *(sqrt(z-1) + z/(sqrt(z-1))) 
+se z>0 oppure 1 se z=0. 
+Scrivere un programma che acquisisca da tastiera un intero non negativo 
+e restituisca l'approssimazione della sua radice quadrata calcolata tramite il 
+suddetto metodo implementato tramite una funzione approxsqrt. 
+Il risultato deve essere stampato con esattamente 4 cifre decimali.
 input:	86	output:	9.2738	*/
+
 #include<stdio.h>
 float approxsqrt(int z){
-	if(z==0)
-		return 1;
-	else if(z>0)
-		return (approxsqrt(z-1)+(z/approxsqrt(z-1)))/2;
+	if(z==0)		return 1;
+	else if(z>0)		return (approxsqrt(z-1)+(z/approxsqrt(z-1)))/2;
 }
 int main(){
 	int x;
@@ -13,4 +20,3 @@ int main(){
 	printf("%.4f\n",approxsqrt(x));
 	return 0;
 }
-
