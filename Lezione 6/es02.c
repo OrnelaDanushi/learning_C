8e80a64c923dd68d64aa4a7683167fbf2d8c90fa
Ornela Danushi <orneladanushi@gmail.com>
2023-04-02 22:08:16 +0200

    Update es02.c

--- Lezione 6/es02.c
+++ Lezione 6/es02.c
@@ -1,31 +1,32 @@
-/*FUNZIONE SWAP ORDINATO: Si realizzi un programma che legga 3 interi da standard input ed applichi la funzione a 3 argomenti ordered_swap(-,-,-) alle 3 variabili in cui sono stati memorizzati i 3 valori. I valori letti devono essere passati alla funzione nell'ordine in cui vengono acquisiti da tastiera. Si implementi la funzione ordered_swap, che scambia il contenuto delle 3 variabili di input in modo che la variabile corrispondente al primo argomento contenga l'intero minore, la seconda variabile contenga l'intero mediano e la terza contenga l'intero di valore maggiore. Si stampi a video il contenuto delle 3 variabili all'uscita dall'esecuzione della funzione.
+/*FUNZIONE SWAP ORDINATO: 
+Si realizzi un programma che legga 3 interi da standard input ed applichi 
+la funzione a 3 argomenti ordered_swap(-,-,-) alle 3 variabili in cui sono stati memorizzati i 3 valori. 
+I valori letti devono essere passati alla funzione nell'ordine in cui vengono acquisiti da tastiera. 
+Si implementi la funzione ordered_swap, che scambia il contenuto delle 3 variabili di input in modo che 
+la variabile corrispondente al primo argomento contenga l'intero minore, 
+la seconda variabile contenga l'intero mediano e la terza contenga l'intero di valore maggiore. 
+Si stampi a video il contenuto delle 3 variabili all'uscita dall'esecuzione della funzione.
 Input:	14	-1	7	Output:	-1	7	14	*/
+
 #include<stdio.h>
 int ordered1(int x, int y, int z){
 	int min=x;
-	if(y<x)
-		min=y;
-	else if(z<min)
-		min=z;
+	if(y<x)		min=y;
+	else if(z<min)	min=z;
 	return min;
 }
 int ordered2(int x, int y, int z){
 	int max=z;
-	if(y>z)
-		max=y;
-	else if(x>max)
-		max=x;
+	if(y>z)		max=y;
+	else if(x>max)	max=x;
 	return max;
 }
 int ordered(int x, int y, int z){
 	int minimum=ordered1(x,y,z);
 	int maximum=ordered2(x,y,z);	
-	if(x!=minimum && x!=maximum)
-		return x;
-	else if(y!=minimum && y!=maximum)
-		return y;
-	else if(z!=minimum && z!=maximum)
-		return z;
+	if(x!=minimum && x!=maximum)		return x;
+	else if(y!=minimum && y!=maximum)	return y;
+	else if(z!=minimum && z!=maximum)	return z;
 }
 int main(){
 	int a, b, c;
