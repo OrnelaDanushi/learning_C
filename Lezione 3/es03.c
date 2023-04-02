c37bdd55faae6f42d9a7dc3ef31dcf755fc51ec5
Ornela Danushi <orneladanushi@gmail.com>
2023-04-02 21:21:28 +0200

    Update es03.c

--- Lezione 3/es03.c
+++ Lezione 3/es03.c
@@ -1,6 +1,9 @@
-/*AVERAGE: Scrivere un programma che legga da tastiera tre numeri (anche con la virgola) e stampi in uscita la media, riportandola con tre cifre decimali.
+/*AVERAGE: 
+Scrivere un programma che legga da tastiera tre numeri (anche con la virgola) 
+e stampi in uscita la media, riportandola con tre cifre decimali.
 Input:	1	2	3		Output:	2.000
 Input:	10.5	23.46	31.24		Output:	21.733	*/
+
 #include<stdio.h>
 int main(){
 	float x, y, z, media;
@@ -11,4 +14,3 @@ int main(){
 	printf("%.3f\n",media); /*oppure printf("%1.3f\n",media);*/
 	return 0;
 }
-
