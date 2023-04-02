631f6747a7bdafc3f8796d27fa13eb2fa95de1f7
Ornela Danushi <orneladanushi@gmail.com>
2023-04-02 22:02:12 +0200

    Update es04.c

--- Lezione 5/es04.c
+++ Lezione 5/es04.c
@@ -1,13 +1,15 @@
-/*MCD & MCM: Si realizzi un programma in C che acquisisca da tastiera 2 numeri interi N ed M e utilizzi 2 funzioni per calcolare il minimo comune multiplo e il massimo comune divisore tra N ed M. Il programma stampa sullo standard output prima il mcd(n,m) e poi il mcm(n,m).
+/*MCD & MCM: 
+Si realizzi un programma che acquisisca da tastiera 2 numeri interi N ed M 
+e utilizzi 2 funzioni per calcolare il minimo comune multiplo 
+e il massimo comune divisore tra N ed M. Il programma stampa sullo standard output 
+prima il mcd(n,m) e poi il mcm(n,m).
 input:	2	5	output:	1	10	*/
+
 #include<stdio.h>
 int mcd(int x, int y){
-	if(x==y)
-		return x;
-	if(x<y)
-		return mcd(x,y-x);
-	else
-		return mcd(x-y,y);
+	if(x==y)	return x;
+	if(x<y)		return mcd(x,y-x);
+	else		return mcd(x-y,y);
 }
 int mcm(int x, int y){
 	int prodotto=x*y;
