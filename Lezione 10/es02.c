3ad0ced5f3321dc2b48cc645f2618dad5e4cb45d
Ornela Danushi <orneladanushi@gmail.com>
2023-04-03 13:55:24 +0200

    Update es02.c

--- Lezione 10/es02.c
+++ Lezione 10/es02.c
@@ -1,9 +1,34 @@
 /*Dato lo scheletro di codice qui riportato, lo si completi aggiungendo l'implementazione delle funzioni richieste.
+
 #include<stdio.h>
 #include<stdlib.h>
-int *leggiInput(int *dim);	//funzioni da implementare
+
+// legge una sequenza di interi (positivi e negativi) terminata da uno 0 e li inserisce in un array nell'ordine in cui vengono letti. 
+// Lo 0 comparira' solo come terminatore della sequenza e non dovra' essere inserito nell'array. 
+// Si puo' assumere che la sequenza possa prevedere al massimo 20 elementi, escluso lo 0. 
+// La funzione restituisce l'array e scrive, nell'indirizzo puntato da dim, la dimensione dell'array letto 
+// (ovvero il numero di elementi letti da tastiera diversi da 0);
+int *leggiInput(int *dim);	
+
+// accetta come parametri un array di interi vect, la sua dimensione dim, e un puntatore ad interi new_dim. 
+// modifica l'array vect sostituendo ogni occorrenza di un numero negativo con il suo valore assoluto ed aggiungendo 
+// in fondo all'array un elemento contenente, come il precedente, il valore assoluto del numero negativo. 
+// Supponendo che il primo numero negativo si trovi in posizione i nell'array vect, ne consegue che il valore di vect[i] verra' 
+// trasformato in positivo, mentre verra' aggiunto un ulteriore elemento abs(vect[i])in coda all'array. 
+// L'inserimento degli elementi in coda all'array deve avvenire rispettando l'ordine dei corrispondenti numeri negativi nel vettore vect iniziale. 
+// Ad esempio, dato l'array {4, -1, 2, -3}, questo verra' trasformato nell'array {4, 1, 2, 3, 1, 3}. 
+// La funzione restituisce il nuovo array dopo il ridimensionamento e scrive, nell'indirizzo puntato da new_dim, la dimensione del nuovo array.
 int *eliminaNeg(int *vect, int dim, int *newdim);
+
+// accetta come parametri un array di interi vect e la sua dimensione dim. 
+// Restituisce un array di float, il cui elemento i-esimo contiene la media tra gli elementi di vect in posizione 
+// i a partire dall'inizio e dalla fine dell'array. Ad esempio, in posizione i a partire dall'inizio e dalla fine dell'array. 
+// Ad esempio, in posizione 0 sara' contenuta la media tra il primo e l'ultimo elemento di vect, 
+// in posizione 1 tra il secondo e il penultimo, 
+// in posizione 2 tra il terzo e il terz'ultimo, etc. 
+// In caso l'array vect sia di dimensione dispari, ricopiare l'elemento centrale come ultimo valore dell'array delle medie.
 float *mediaEstremi(int *vect, int dim);
+
 int main(){
 	int *input, *noneg, dim, dimnoneg, i;
 	float *results;
@@ -19,15 +44,16 @@ int main(){
 	for(i=0; i<dim; i++)	printf("%3.2f\n,results[i]");
 	return 0;
 }
-Le funzioni devono implementare le seguenti operazioni:
--leggiInput: legge una sequenza di interi (positivi e negativi) terminata da uno 0 e li inserisce in un array nell'ordine in cui vengono letti. Lo 0 comparira' solo come terminatore della sequenza e non dovra' essere inserito nell'array. Si puo' assumere che la sequenza possa prevedere al massimo 20 elementi, escluso lo 0. La funzione restituisce l'array e scrive, nell'indirizzo puntato da dim, la dimensione dell'arrau letto (ovvero il numero di elementi letti da tastiera diversi da 0);
--eliminaNeg: la funzione accetta come parametri un array di interi vect, la sua dimensione dim, e un puntatore ad interi new_dim. La funzione modifica l'array vect sostituendo ogni occorrenza di un numero negativo con il suo valore assoluto ed aggiungendo in fondo all'array un elemento contenente, come il precedente, il valore assoluto del numero negativo. Supponendo che il primo numero negativo si trovi in posizione i nell'array vect, ne consegue che il valore di vect[i] verra' trasformato in positivo, mentre verra' aggiunto un ulteriore elemento abs(vect[i])in coda all'array. L'inserimento degli elementi in coda all'array deve avvenire rispettando l'ordine dei corrispondenti numeri negativi nel vettore vect iniziale. Ad esempio, dato l'array {4, -1, 2, -3}, questo verra' trasformato nell'array {4, 1, 2, 3, 1, 3}. La funzione restituisce il nuovo array dopo il ridimensionamento e scrive, nell'indirizzo puntato da new_dim, la dimensione del nuovo array.
--mediaEstremi: la funzione accetta come parametri un array di interi vect e la sua dimensione dim. Restituisce un array di float, il cui elemento i-esimo contiene la media tra gli elementi di vect in posizione i a partire dall'inizio e dalla fine dell'array. Ad esempio, in posizione i a partire dall'inizio e dalla fine dell'array. Ad esempio, in posizione 0 sara' contenuta la media tra il primo e l'ultimo elemento di vect, in posizione 1 tra il secondo e il penultimo, in posizione 2 tra il terzo e il terz'ultimo, etc. In caso l'array vect sia di dimensione dispari, ricopiare l'elemento centrale come ultimo valore dell'array delle medie.
 input:	2	-1	4	7	-8	1	3	0	
-output:	Stampa dei valori in input: (7 valori)	2	-1	4	7	-8	1	3	Stampa dei valori senza negativi: (9 valori)	2	1	4	7	8	1	3	1	8	Risultato di mediaEstremi:	5.00	1.00	3.50	4.00	8.00	*/
+output:	
+Stampa dei valori in input: (7 valori)	2	-1	4	7	-8	1	3	
+Stampa dei valori senza negativi: (9 valori)	2	1	4	7	8	1	3	1	8	
+Risultato di mediaEstremi:	5.00	1.00	3.50	4.00	8.00	*/
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
+
 int *leggiInput(int *dim){
 	int *a=malloc(20*sizeof(int)), i=0, dim2=0, n;
 	scanf("%d", &n);
