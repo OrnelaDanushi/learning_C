c5a704f536336fe4d8d8c6bd559988a57b4a15ca
Ornela Danushi <orneladanushi@gmail.com>
2023-04-03 15:01:13 +0200

    Create 08.c

---
+++ da_slide/08.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define MATRICOLA 536295
+int main(){
+	int a; 
+a=19;	
+	printf("Ciao Mondo,ho %d anni e il mio numero di matricola e'%d\n",a,MATRICOLA);
+	scanf("%d",&a);	
+	return 0;
+}
